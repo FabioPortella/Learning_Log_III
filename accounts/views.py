5177fc4936083b981d66da815a8619697b4bf3b4
FabioPortella <fabioportella2009@gmail.com>
2023-06-27 19:55:45 -0300

    Cadastro de um novo usuário

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    """Cadastro de um novo usuário"""
+    if request.method != 'POST':
+        # Exibe o formulario em branco de cadastro
+        form = UserCreationForm()
+    else:
+        # Processa o formulário preenchido.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Faz o login do usuário e o redireciona para a página inicial
+            login(request, new_user)
+            return redirect('learning_logs:index')
+        
+    # Exibe o formulário em branco ou inválido
+    context = {
+        'form': form
+    }
+    return render(request, 'registration/register.html', context)
