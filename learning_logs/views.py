d747dc0426b9dc7e31755773aaead109b9501de1
FabioPortella <fabioportella2009@gmail.com>
2023-06-27 20:03:26 -0300

    restringindo acesso as paginas com @login_required

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+
 
 def index(request):
     return render(request, 'learning_logs/index.html')
 
 
+@login_required
 def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {
@@ -14,6 +17,7 @@ def topics(request):
     return render (request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """Mostra um único tópico e todas as suas entradas"""
     topic = Topic.objects.get(id=topic_id)
@@ -25,6 +29,7 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """Adiciona um tópico novo"""
     if request.method != 'POST':
@@ -44,6 +49,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Adiciona uma entrada nova para um tópico especifico"""
     topic = Topic.objects.get(id = topic_id)
@@ -67,6 +73,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edita uma entrada existente"""
     entry = Entry.objects.get(id=entry_id)
