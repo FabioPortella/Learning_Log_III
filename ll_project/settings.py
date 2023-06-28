98b597665bc9fde09d75a8ae20e9e4b4c512a8d8
FabioPortella <fabioportella2009@gmail.com>
2023-06-28 20:41:39 -0300

    estilizando pagina com django-bootstrap5

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'accounts',
 
+    # Aplicações de terceiros
+    'django_bootstrap5',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -108,11 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'pt-br'
-
 TIME_ZONE = 'America/sao_paulo'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
